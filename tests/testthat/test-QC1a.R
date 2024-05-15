6b0d3cbf27215f3131022b75f620b7c1ac78b56f
Soria van Uffelen <uffelevs@rivm-rsrv-l01p.rivm.ssc-campus.nl>
2024-05-15 17:19:43 +0200

    Hernoemen QC1_new_a naar QC1_a #90

--- tests/testthat/test-QC1a.R
+++ tests/testthat/test-QC1a.R
@@ -1,16 +1,16 @@
-test_that("QC1_new_a", {
+test_that("QC1a", {
 
 
               data(metingen)
               data(veld)
 
-              x <- QC1_new_a(d_veld = veld, d_metingen = metingen)
+              x <- QC1a(d_veld = veld, d_metingen = metingen)
 
               # test if attributes exist
               expect_true(qcout_attrexists(x))
               x_attr <- attr(x, "qcout")
-              expect_false(is.null(x_attr[["QC1_new_a"]]))
-              expect_true(is.list(x_attr[["QC1_new_a"]][["resultaat"]]))
+              expect_false(is.null(x_attr[["QC1a"]]))
+              expect_true(is.list(x_attr[["QC1a"]][["resultaat"]]))
 
               expect_true(nrow(metingen) == nrow(x))
 
