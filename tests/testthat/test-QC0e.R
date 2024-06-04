f56db955122cbcf3519293b8dc2359b9ddac24fa
Soria van Uffelen <uffelevs@rivm-rsrv-l01p.rivm.ssc-campus.nl>
2024-06-04 09:46:22 +0200

    Updaten en hernoemen test-QC0f naar test-QC0e #90

--- tests/testthat/test-QC0e.R
+++ tests/testthat/test-QC0e.R
@@ -1,22 +1,22 @@
 
-test_that("QC0f",{
+test_that("QC0e",{
 
 
               data(veld)
               data(filter)
               data(metingen)
 
-              x <- QC0f(d_veld=veld,d_filter=filter,d_metingen=metingen)
+              x <- QC0e(d_veld=veld,d_filter=filter,d_metingen=metingen)
 
               # test if attributes exist
               expect_true(qcout_attrexists(x))
               x_attr <- attr(x,"qcout")
-              expect_false(is.null(x_attr[["QC0f"]]))
-              expect_true(is.list(x_attr[["QC0f"]][["resultaat"]]))
+              expect_false(is.null(x_attr[["QC0e"]]))
+              expect_true(is.list(x_attr[["QC0e"]][["resultaat"]]))
 
 
               # test if ids are from metingen data.frame
-              ids <- x_attr[["QC0f"]][["oordeel"]][["twijfelachtig"]]
+              ids <- x_attr[["QC0e"]][["oordeel"]][["twijfelachtig"]]
               qcids <- metingen$qcid
               v1 <- intersect(ids,qcids)
               expect_true(length(v1)>0)
